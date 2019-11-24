94cea3657b5cba5786f951aa4011daa584bde9a3
Viet Anh Tong <vietanhtong1508@gmail.com>
2019-11-24 10:55:40 +0700

    feat: add Product page

--- frontend/src/components/admin/Navbar.vue
+++ frontend/src/components/admin/Navbar.vue
@@ -4,31 +4,41 @@
       <v-layout column align-center>
         <v-flex class="mt-5">
           <v-avatar size="100">
-            <v-img src="/avatar-1.png"/>
+            <v-img src="/avatar-1.png" />
           </v-avatar>
           <p class="white--text subheading mt-1">Viet Anh</p>
         </v-flex>
       </v-layout>
       <v-list>
-        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+        <v-list-tile
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
           <v-list-tile-action>
             <v-icon class="white--text">{{ link.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+            <v-list-tile-title class="white--text">
+              {{ link.text }}
+            </v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
 
     <v-toolbar flat app>
-      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase">
         <span class="font-weight-light mr-1">Admin</span>
         <span>management</span>
       </v-toolbar-title>
 
-      <v-spacer/>
+      <v-spacer />
 
       <v-btn flat color="grey">
         <span class="mr-1">Sign out</span>
@@ -44,10 +54,9 @@ export default {
     return {
       drawer: null,
       links: [
-        { icon: "dashboard", text: "Category", route: "/admin/category" },
+        { icon: 'dashboard', text: 'Category', route: '/admin/category' },
         { icon: 'branding_watermark', text: 'Brand', route: '/admin/brand' },
-        { icon: "smartphone", text: "Smartphone", route: "/admin/smartphone" },
-        { icon: "laptop", text: "Laptop", route: "/admin/laptop" }
+        { icon: 'laptop', text: 'Product', route: '/admin/product' }
       ]
     };
   }
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,6 +4,7 @@ import Home from '@/views/Home';
 import Admin from '@/views/admin/Admin';
 import Category from '@/views/admin/Category';
 import Brand from '@/views/admin/Brand';
+import Product from '@/views/admin/Product';
 
 Vue.use(Router);
 
@@ -26,6 +27,10 @@ export default new Router({
         {
           path: 'brand',
           component: Brand
+        },
+        {
+          path: 'product',
+          component: Product
         }
       ]
     }
