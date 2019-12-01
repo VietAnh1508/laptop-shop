3c98ab08028f2f6d60d33c0d7358270fc80a6d3c
Viet Anh Tong <vietanhtong1508@gmail.com>
2019-12-01 23:20:17 +0700

    feat: rename file before storing

--- backend/src/main/java/com/laptopshop/service/impl/FileStorageServiceImpl.java
+++ backend/src/main/java/com/laptopshop/service/impl/FileStorageServiceImpl.java
@@ -1,7 +1,7 @@
 package com.laptopshop.service.impl;
 
-import com.laptopshop.exception.FileStorageException;
 import com.laptopshop.exception.FileNotFoundException;
+import com.laptopshop.exception.FileStorageException;
 import com.laptopshop.properties.FileStorageProperties;
 import com.laptopshop.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +18,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class FileStorageServiceImpl implements FileStorageService {
@@ -43,22 +45,41 @@ public class FileStorageServiceImpl implements FileStorageService {
     public String storeFile(MultipartFile file) {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
+        validateFile(file, fileName);
+
         try {
-            if (file.isEmpty()) {
-                throw new FileStorageException("Failed to store empty file " + fileName);
-            }
+            String newFileName = generateNewFileName(fileName);
 
-            if (fileName.contains("..")) {
-                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
-            }
-
-            Path targetLocation = fileStorageLocation.resolve(fileName);
+            Path targetLocation = fileStorageLocation.resolve(newFileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 
-            return fileName;
+            return newFileName;
         } catch (IOException e) {
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
         }
+    }
+
+    private void validateFile(MultipartFile file, String fileName) {
+        if (file.isEmpty()) {
+            throw new FileStorageException("Failed to store empty file " + fileName);
+        }
+
+        if (fileName.contains("..")) {
+            throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+        }
+    }
+
+    private String generateNewFileName(String originalFileName) {
+        String newFileName = UUID.randomUUID().toString().replace("-", "");
+        Optional<String> extension = getFileExtension(originalFileName);
+        newFileName += extension.map(s -> ("." + s)).orElse("");
+        return newFileName;
+    }
+
+    private Optional<String> getFileExtension(String fileName) {
+        return Optional.ofNullable(fileName)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(fileName.lastIndexOf('.') + 1));
     }
 
     @Override
